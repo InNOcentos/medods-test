1d74680ca83bf8102121057264de5f715bb5b313
sboris <smrnvbrs@gmail.com>
2020-12-09 14:12:17 +0300

    initil. added nodemon, mongodb, express and jwt

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const { PORT } = require("./utils/constants");
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening on ${PORT}`);
+});
